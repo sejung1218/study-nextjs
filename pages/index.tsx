ef2abe7d61ce9c237a329329dd970e3c45a16c4f
sejung1218 <sejung1218@hanmail.net>
2022-11-11 15:14:58 +0900

    button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
-import { Box } from '@mui/material';
+import { Box, Button, styled, Typography } from '@mui/material';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return <Box>asd123</Box>;
+  return (
+    <>
+      <Box>asd123</Box>
+      <SubmitBtn variant="contained" type="button" color="error">
+        <Typography>ONE</Typography>
+      </SubmitBtn>
+      <SubmitBtn variant="contained" type="button" color="secondary">
+        <Typography>TWO</Typography>
+      </SubmitBtn>
+    </>
+  );
 }
+const SubmitBtn = styled(Button)`
+  width: 60px;
+  height: 30px;
+  margin: 0 0 0 5px;
+`;
