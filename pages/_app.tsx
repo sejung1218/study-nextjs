4659e633f21a1f7628ec490119f0c9b8ea381329
sejung1218 <sejung1218@hanmail.net>
2022-11-15 14:15:04 +0900

    kakao login test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import TestComponent from './component/TestComponent';
+import Head from 'next/head';
+import * as React from 'react';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   // export default function App() {
   // return <div>Hello</div>;
+  <Head>
+    <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
+  </Head>
   return <Component {...pageProps} />;
   // return <TestComponent />;
 }
+
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,51 @@
-import { Box, Button, styled, Typography } from '@mui/material';
-import Link from 'next/link';
+import { Box, Button, styled, Typography } from "@mui/material";
+import Link from "next/link";
+import { Router } from "next/router";
+import { kakaoInit } from "./kakao";
 
 export default function Home() {
-  const testProps = 'TEST_MESSAGE';
+  // const testProps = 'TEST_MESSAGE';
+  // console.log('testProps : ', testProps);
 
-  console.log('testProps : ', testProps);
+  const kakaoLogin = async () => {
+    // 카카오 초기화
+    const kakao = kakaoInit();
+    // 카카오 로그인 구현
+    kakao.Auth.login({
+      success: () => {
+        kakao.API.request({
+          url: "/v2/user/me", // 사용자 정보 가져오기
+          success: (res: any) => {
+            // 로그인 성공할 경우 정보 확인 후 /kakao 페이지로 push
+            console.log(res);
+            Router.push("/kakao");
+          },
+          fail: (error: any) => {
+            console.log(error);
+          },
+        });
+      },
+      fail: (error: any) => {
+        console.log(error);
+      },
+    });
+    // setting complete
 
-  // setting complete
-
-  return (
-    <>
-      <SubmitBtn variant="contained" type="button" color="error">
-        <Link href={'/test01'} as={'tost01'}>
-          <Typography>ONE</Typography>
-        </Link>
-      </SubmitBtn>
-      <SubmitBtn variant="contained" type="button" color="secondary">
-        <Link href={'/test02'} as={'tost02'}>
-          <Typography>TWO</Typography>
-        </Link>
-      </SubmitBtn>
-    </>
-  );
+    return (
+      <>
+        <SubmitBtn variant="contained" type="button" color="error">
+          <Link href={"/test01"} as={"tost01"}>
+            <Typography>ONE</Typography>
+          </Link>
+        </SubmitBtn>
+        <SubmitBtn variant="contained" type="button" color="secondary">
+          <Link href={"/test02"} as={"tost02"}>
+            <Typography>TWO</Typography>
+          </Link>
+        </SubmitBtn>
+      </>
+    );
+  };
 }
 
 const SubmitBtn = styled(Button)`
