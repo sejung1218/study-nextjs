ca2bc186f239869d0b06b9ba9c5733d989c4f868
sejung1218 <sejung1218@hanmail.net>
2022-11-17 13:46:20 +0900

    react-dropzone test02

--- pages/fileupload/index.tsx
+++ pages/fileupload/index.tsx
@@ -1,3 +1,4 @@
+import { Box, Button, Container, styled, Typography } from "@mui/material";
 import { useCallback } from "react";
 import { useDropzone } from "react-dropzone";
 
@@ -9,13 +10,34 @@ export default function FileUpload() {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <div {...getRootProps()} style={{ border: "1px solid black" }}>
+    <FileUploadContainer {...getRootProps()}>
       <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
-      )}
-    </div>
+      {
+        isDragActive ? (
+          <p>Drop the files here ...</p>
+        ) : (
+          <p>Drag 'n' drop some files here, or click to select files</p>
+        )
+        //   <FileUploadBtn variant="contained" color="secondary">
+        //     파일업로드
+        //   </FileUploadBtn>
+      }
+    </FileUploadContainer>
   );
 }
+
+const FileUploadContainer = styled(Container)`
+  display: flex;
+  justify-content: center;
+  /* vertical-align: middle; */
+  align-items: center;
+  flex-direction: column;
+  margin-top: 20px;
+  border: 1px solid black;
+  width: 250px;
+  height: 250px;
+`;
+
+const FileUploadBtn = styled(Button)`
+  color: white;
+`;
