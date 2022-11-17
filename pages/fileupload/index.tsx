2f7cf0c37b8f1b4783a64ff14283adb05226f91e
sejung1218 <sejung1218@hanmail.net>
2022-11-17 14:23:07 +0900

    react-dropzone test02 - file upload fail

--- pages/fileupload/index.tsx
+++ pages/fileupload/index.tsx
@@ -1,28 +1,33 @@
 import { Box, Button, Container, styled, Typography } from "@mui/material";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 export default function FileUpload() {
+  const [fileName, setFileName] = useState();
   const onDrop = useCallback((acceptedFiles: any) => {
     // Do something with the files
+    setFileName(acceptedFiles);
     console.log(acceptedFiles);
   }, []);
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
-    <FileUploadContainer {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <p>Drag 'n' drop some files here, or click to select files</p>
-        )
-        //   <FileUploadBtn variant="contained" color="secondary">
-        //     파일업로드
-        //   </FileUploadBtn>
-      }
-    </FileUploadContainer>
+    <Container>
+      <FileUploadContainer {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <p>Drag 'n' drop some files here, or click to select files</p>
+          )
+          //   <FileUploadBtn variant="contained" color="secondary">
+          //     파일업로드
+          //   </FileUploadBtn>
+        }
+      </FileUploadContainer>
+      <Typography>{fileName}1</Typography>
+    </Container>
   );
 }
 
