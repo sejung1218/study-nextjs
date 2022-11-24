aa0ad84f32eed758dd361478fe7ae96e1e836911
sejung1218 <sejung1218@hanmail.net>
2022-11-24 21:43:07 +0900

    remove useEffect file

--- pages/fileupload1/index.tsx
+++ pages/fileupload1/index.tsx
@@ -3,11 +3,13 @@ import { useCallback, useEffect, useState } from "react";
 import { useDropzone } from "react-dropzone";
 
 export default function FileUpload1() {
-  const [fileName, setFileName] = useState();
-  const [file, setFile] = useState<File[]>();
+  // react-dropzone file.name 으로 관리가 되기 때문에 setFileName 은 필요가 없다.
+  // const [fileName, setFileName] = useState();
+  // react-dropzone acceptedFiles 으로 관리가 되기 때문에 setFile 은 필요가 없다.
+  // const [file, setFile] = useState<File[]>();
 
   const onDrop = useCallback((acceptedFiles: any) => {
-    setFileName(acceptedFiles[0].path);
+    // setFileName(acceptedFiles[0].path);
     console.log("acceptedFiles1 : ", acceptedFiles);
   }, []);
 
@@ -26,10 +28,12 @@ export default function FileUpload1() {
     },
   });
 
-  useEffect(() => {
-    setFile(acceptedFiles);
-    console.log("file : ", file);
-  }, [file, acceptedFiles]);
+  // react-dropzone은 acceptedFiles 가 파일의 상태관리를 해주고 있으므로 useState를 사용해서 파일의 상태를 관리해줄 필요가 없다.
+
+  // useEffect(() => {
+  //   setFile(acceptedFiles);
+  //   console.log("file : ", file);
+  // }, [file, acceptedFiles]);
 
   const acceptedFileItems = acceptedFiles.map((file) => (
     <li key={file.name}>
@@ -63,7 +67,7 @@ export default function FileUpload1() {
           //   </FileUploadBtn>
         }
       </FileUploadContainer>
-      <Typography>{fileName}</Typography>
+      {/* <Typography>{fileName}</Typography> */}
       <h4>Accepted files</h4>
       <ul>{acceptedFileItems}</ul>
       <h4>Rejected files</h4>
